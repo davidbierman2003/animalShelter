aa05b415b8a34203f27220ae1cb9051be1e9f3a1
davidbierman2003 <davidbierman2003@gmail.com>
2022-04-25 17:15:50 -0600

    Added a way to input vaccine todo

--- FrontEndAnimalShelter/ToDoForm.Designer.cs
+++ FrontEndAnimalShelter/ToDoForm.Designer.cs
@@ -33,6 +33,17 @@
             this.dgVaccineToDo = new System.Windows.Forms.DataGridView();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.lblMedication = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBoxAnimalID = new System.Windows.Forms.TextBox();
+            this.Vacine = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtBoxVaccineID = new System.Windows.Forms.TextBox();
+            this.txtBoxEmployeeID = new System.Windows.Forms.TextBox();
+            this.btnSubmitVaccine = new System.Windows.Forms.Button();
+            this.dateGiven = new System.Windows.Forms.DateTimePicker();
+            this.dateNextDateDue = new System.Windows.Forms.DateTimePicker();
             ((System.ComponentModel.ISupportInitialize)(this.dgVaccineToDo)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
@@ -41,61 +52,168 @@
             // 
             this.headerDate.AutoSize = true;
             this.headerDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.headerDate.Location = new System.Drawing.Point(916, 87);
+            this.headerDate.Location = new System.Drawing.Point(515, 56);
+            this.headerDate.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.headerDate.Name = "headerDate";
-            this.headerDate.Size = new System.Drawing.Size(252, 78);
+            this.headerDate.Size = new System.Drawing.Size(155, 47);
             this.headerDate.TabIndex = 0;
             this.headerDate.Text = "header";
             // 
             // lblVaccine
             // 
             this.lblVaccine.AutoSize = true;
-            this.lblVaccine.Location = new System.Drawing.Point(51, 271);
+            this.lblVaccine.Location = new System.Drawing.Point(29, 175);
+            this.lblVaccine.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblVaccine.Name = "lblVaccine";
-            this.lblVaccine.Size = new System.Drawing.Size(131, 32);
+            this.lblVaccine.Size = new System.Drawing.Size(74, 20);
             this.lblVaccine.TabIndex = 1;
             this.lblVaccine.Text = "Vaccines";
             // 
             // dgVaccineToDo
             // 
             this.dgVaccineToDo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgVaccineToDo.Location = new System.Drawing.Point(57, 307);
+            this.dgVaccineToDo.Location = new System.Drawing.Point(32, 198);
+            this.dgVaccineToDo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dgVaccineToDo.Name = "dgVaccineToDo";
             this.dgVaccineToDo.RowHeadersWidth = 102;
             this.dgVaccineToDo.RowTemplate.Height = 40;
-            this.dgVaccineToDo.Size = new System.Drawing.Size(1561, 355);
+            this.dgVaccineToDo.Size = new System.Drawing.Size(878, 229);
             this.dgVaccineToDo.TabIndex = 2;
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(57, 770);
+            this.dataGridView1.Location = new System.Drawing.Point(32, 497);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 102;
             this.dataGridView1.RowTemplate.Height = 40;
-            this.dataGridView1.Size = new System.Drawing.Size(1561, 355);
+            this.dataGridView1.Size = new System.Drawing.Size(878, 229);
             this.dataGridView1.TabIndex = 4;
             // 
             // lblMedication
             // 
             this.lblMedication.AutoSize = true;
-            this.lblMedication.Location = new System.Drawing.Point(51, 734);
+            this.lblMedication.Location = new System.Drawing.Point(29, 474);
+            this.lblMedication.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblMedication.Name = "lblMedication";
-            this.lblMedication.Size = new System.Drawing.Size(168, 32);
+            this.lblMedication.Size = new System.Drawing.Size(94, 20);
             this.lblMedication.TabIndex = 3;
             this.lblMedication.Text = "Medications";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(71, 114);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Animal ID";
+            // 
+            // txtBoxAnimalID
+            // 
+            this.txtBoxAnimalID.Location = new System.Drawing.Point(155, 111);
+            this.txtBoxAnimalID.Name = "txtBoxAnimalID";
+            this.txtBoxAnimalID.Size = new System.Drawing.Size(168, 26);
+            this.txtBoxAnimalID.TabIndex = 6;
+            // 
+            // Vacine
+            // 
+            this.Vacine.AutoSize = true;
+            this.Vacine.Location = new System.Drawing.Point(359, 116);
+            this.Vacine.Name = "Vacine";
+            this.Vacine.Size = new System.Drawing.Size(66, 20);
+            this.Vacine.TabIndex = 7;
+            this.Vacine.Text = "Vaccine";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(625, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Employee ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(107, 157);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Date Given";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(529, 158);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(114, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Next Date Due";
+            // 
+            // txtBoxVaccineID
+            // 
+            this.txtBoxVaccineID.Location = new System.Drawing.Point(436, 114);
+            this.txtBoxVaccineID.Name = "txtBoxVaccineID";
+            this.txtBoxVaccineID.Size = new System.Drawing.Size(168, 26);
+            this.txtBoxVaccineID.TabIndex = 11;
+            // 
+            // txtBoxEmployeeID
+            // 
+            this.txtBoxEmployeeID.Location = new System.Drawing.Point(742, 114);
+            this.txtBoxEmployeeID.Name = "txtBoxEmployeeID";
+            this.txtBoxEmployeeID.Size = new System.Drawing.Size(168, 26);
+            this.txtBoxEmployeeID.TabIndex = 12;
+            // 
+            // btnSubmitVaccine
+            // 
+            this.btnSubmitVaccine.Location = new System.Drawing.Point(926, 289);
+            this.btnSubmitVaccine.Name = "btnSubmitVaccine";
+            this.btnSubmitVaccine.Size = new System.Drawing.Size(134, 40);
+            this.btnSubmitVaccine.TabIndex = 13;
+            this.btnSubmitVaccine.Text = "Submit Vaccine";
+            this.btnSubmitVaccine.UseVisualStyleBackColor = true;
+            this.btnSubmitVaccine.Click += new System.EventHandler(this.btnSubmitVaccine_Click);
+            // 
+            // dateGiven
+            // 
+            this.dateGiven.Location = new System.Drawing.Point(202, 154);
+            this.dateGiven.Name = "dateGiven";
+            this.dateGiven.Size = new System.Drawing.Size(282, 26);
+            this.dateGiven.TabIndex = 14;
+            // 
+            // dateNextDateDue
+            // 
+            this.dateNextDateDue.Location = new System.Drawing.Point(650, 155);
+            this.dateNextDateDue.Name = "dateNextDateDue";
+            this.dateNextDateDue.Size = new System.Drawing.Size(300, 26);
+            this.dateNextDateDue.TabIndex = 15;
+            // 
             // ToDoForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1924, 1282);
+            this.ClientSize = new System.Drawing.Size(1082, 714);
+            this.Controls.Add(this.dateNextDateDue);
+            this.Controls.Add(this.dateGiven);
+            this.Controls.Add(this.btnSubmitVaccine);
+            this.Controls.Add(this.txtBoxEmployeeID);
+            this.Controls.Add(this.txtBoxVaccineID);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Vacine);
+            this.Controls.Add(this.txtBoxAnimalID);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.lblMedication);
             this.Controls.Add(this.dgVaccineToDo);
             this.Controls.Add(this.lblVaccine);
             this.Controls.Add(this.headerDate);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "ToDoForm";
             this.Text = "ToDoForm";
             this.Load += new System.EventHandler(this.ToDoForm_Load);
@@ -113,5 +231,16 @@
         private System.Windows.Forms.DataGridView dgVaccineToDo;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Label lblMedication;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBoxAnimalID;
+        private System.Windows.Forms.Label Vacine;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtBoxVaccineID;
+        private System.Windows.Forms.TextBox txtBoxEmployeeID;
+        private System.Windows.Forms.Button btnSubmitVaccine;
+        private System.Windows.Forms.DateTimePicker dateGiven;
+        private System.Windows.Forms.DateTimePicker dateNextDateDue;
     }
 }
